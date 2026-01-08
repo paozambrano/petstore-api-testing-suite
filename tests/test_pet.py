572aa3e3a05d61ed4ffe4e0b583731920d5f4f79
Pao Zambrano <pao.zambranoo_@hotmail.com>
2026-01-08 16:02:49 -0500

    Fix typo in pet_schema.json and add schema validation test in test_pet.py

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,3 +1,7 @@
+from jsonschema import validate
+import json
+import os
+
 def test_create_and_get_pet(pet_api, pet_data):
     post_response = pet_api.create_pet(pet_data)
     assert post_response.status_code == 200, f"Error: Expected 200 but received {post_response.status_code}"
@@ -13,3 +17,14 @@ def test_create_and_get_pet(pet_api, pet_data):
     assert response_body["status"] == pet_data["status"]
 
     print(f"Validation Successful validation: The name{response_body['name']} matches")
+
+def test_pet_schema_validation(pet_api, pet_data):
+    response = pet_api.create_pet(pet_data)
+    response_json = response.json()
+
+    schema_path = os.path.join(os.path.dirname(__file__),'../schemas/pet_schema.json')
+    with open(schema_path, "r") as file:
+        schema = json.load(file)
+
+    validate(instance=response_json, schema=schema)
+    print("\nThe API contract is valid (JSON Schema matched)")
